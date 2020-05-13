f3ac62b036856427711a0bf249ddc5753a94f4f9
Renan Magalhães Saraiva <renan.m.saraiva@gmail.com>
2020-05-12 21:00:24 -0300

    Update db.config.js

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,9 +1,5 @@
 require('dotenv').config();
 
-// module.exports = {
-//     url: "mongodb+srv://renansaraiva:ocr.1010@discovery-jpanz.mongodb.net/test?retryWrites=true&w=majority"
-// };
-
 module.exports = {
     url: `mongodb+srv://${process.env.BDUSER}:${process.env.DBPASSWORD}@${process.env.DBURI}`
 };
